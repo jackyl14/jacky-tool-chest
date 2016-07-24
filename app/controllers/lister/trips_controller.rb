4f092f0efb06b1366fba03ee3772637ae33dc21f
Jacky Lee <jackylee.crew@gmail.com>
2016-07-23 17:52:32 -0700

    Created controller & view for lister/trips
    The purpose is create a static page to
    experiment with the design for a list
    of uber rides.

---
+++ app/controllers/lister/trips_controller.rb
@@ -0,0 +1,9 @@
+module Lister
+  class TripsController < ApplicationController
+
+    def index
+      @initial_data_props = {}
+    end
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   namespace :importer do
     resources :upc, only: [:index]
   end
+
+  namespace :lister do
+    resources :trips, only: [:index]
+  end
+  
   root to: 'importer/upc#index'
 end
